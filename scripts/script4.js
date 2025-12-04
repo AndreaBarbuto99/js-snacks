ac6da3b8a195533f2adb016e7952ee6ae799b246
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-04 13:11:42 +0100

    feat: aggiunto js-snack 4 in html4

--- scripts/script4.js
+++ scripts/script4.js
@@ -1,2 +1,17 @@
 /* L’utente inserisce due parole in successione, con due prompt. 
  Il software stampa prima la parola più corta, poi la parola più lunga. */
+
+
+ userWordOne = prompt("inserisci una parola");
+ userWordTwo = prompt("Inserisci una seconda parola");
+
+ if (userWordOne.length > userWordTwo.length)
+ {
+    console.log("la parola più corta è " + userWordTwo);
+    console.log("la parola più lunga è " + userWordOne);
+ }
+
+ else {
+    console.log("la parola più corta è " + userWordOne);
+    console.log("la parola più lunga è " + userWordTwo);
+ }
