766d991dc7c6847a102129c8388665ddf5aeabd6
Andrea Barbuto <barbutoandrea99@gmail.com>
2025-12-04 11:32:16 +0100

    feat: js-snack 1

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+ JSnack 1
+ L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+*/
+
+const numOne = parseInt(prompt("Inserisci il primo numero"));
+const numTwo = parseInt(prompt("Inserisci il secondo numero"));
+
+if(Number.isInteger(numOne) === true && Number.isInteger(numTwo) === true )
+{
+    if (numOne > numTwo)
+    {
+        console.log(numOne);
+    }
+
+    else if(numTwo > numOne)
+    {
+        console.log(numTwo);
+    }
+}
+
+else {
+    console.log("non hai inserito dei numeri");
+}
